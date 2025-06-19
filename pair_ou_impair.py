801896b694e7ca8908843153fbc2d2ba4538bdd4
Dalihana7 <dalinda_djelassi@hotmail.fr>
2025-06-19 11:12:50 +0200

    Add files via upload

---
+++ pair_ou_impair.py
@@ -0,0 +1,13 @@
+'''
+pair_ou_impair
+'''
+
+
+__author__ = "djelas_d"
+
+
+def is_even(n: int) -> bool:
+    '''
+    pair_ou_impair
+    '''
+    return n % 2 == 0  # True si pair, False si  impair
